84a28a9b08ec22278e2d12f60961f5b2bfb97742
alexevan13 <13alexchristian@gmail.com>
2017-08-13 16:36:34 +0530

    v 0.1

---
+++ index.php
@@ -0,0 +1,382 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome To Eliza Fashion</title>
+<!-- Custom Theme files -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Snazzy Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+	Smartphone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //Custom Theme files -->
+<link href="main/css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+<link href="main/css/style.css" type="text/css" rel="stylesheet" media="all">
+<link rel="stylesheet" href="main/css/chocolat.css" type="text/css" media="screen">
+<link rel="stylesheet" href="main/css/flexslider.css" type="text/css" media="screen" />
+<!-- js -->
+<script src="main/js/jquery-1.11.1.min.js"></script> 
+<!-- //js -->
+<!--light-box-files-->
+<script src="main/js/jquery.chocolat.js"></script>
+<script type="text/javascript">
+	$(function() {
+		$('.gallery-grid a').Chocolat();
+	});
+</script>
+<!--//light-box-files-->
+<!--animate-->
+<link href="main/css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="main/js/wow.min.js"></script>
+	<script>
+		 new WOW().init();
+	</script>
+<!--//end-animate-->
+<!-- start-smooth-scrolling-->
+<script type="text/javascript" src="main/js/move-top.js"></script>
+<script type="text/javascript" src="main/js/easing.js"></script>	
+<script type="text/javascript">
+		jQuery(document).ready(function($) {
+			$(".scroll").click(function(event){		
+				event.preventDefault();
+		
+		$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+			});
+		});
+</script>
+<!--//end-smooth-scrolling-->	
+</head>
+<body>
+	<!--banner-->
+	<div class="banner">
+		<div class="banner-left">
+			<div class="logo wow fadeInDownBig animated" data-wow-delay=".5s">
+				<h1><a href="index.php">Eliza Fashion</a></h1>
+				<p>Simple & Perfect</p>
+			</div>
+			<div class="address wow fadeInUpBig animated" data-wow-delay=".5s">
+				<p>67. B. 309/5. HariKrishna Park</p>
+				<p>B/s Ranchhodnagar, Opp. Urmijivan Soc</p>
+				<p>Vatva Vinzol Road, Vatva, Ahmedabad.</p>
+				<p>E-mail: <a href="mailto:info@example.com">mail@example.com</a></p>
+                                <a href="tel:+918849947902"><p class="phn"><span class="glyphicon glyphicon-earphone"></span>+91 88499 47902</p></a>
+			</div>
+			<img src="main/images/1.png" class="bnrleft-img wow zoomIn animated" data-wow-delay=".5s" alt=""/>
+		</div>
+		<div class="banner-right wow fadeInRight animated" data-wow-delay=".5s">
+			<div class="navbar-left">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				Menu	
+				</button>	
+			</div>
+			<!--navigation-->
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="top-nav">
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav navbar-left cl-effect-5">
+						<li class="active"><a href="index.php"><span data-hover="Home">Home</span></a></li>
+						<li><a href="#about" class="scroll"><span data-hover="About">About</span></a></li>
+						<li><a href="#services" class="scroll"><span data-hover="Production">Production</span></a></li>
+                                                <!--<li><a href="underconstruction.php" target="_blank"><span data-hover="Shop">Shop</span></a></li>
+						--><li><a href="#gallery" class="scroll"><span data-hover="Gallery">Gallery</span></a></li>
+						<li><a href="#contact" class="scroll"><span data-hover="Contact Us">Contact Us</span></a></li>
+					</ul>
+					<div class="clearfix"> </div>
+				</div><!--//navigation-->
+			</div>
+			<div class="bnr-img">
+			</div>
+		</div>
+		<div class="clearfix"> </div>
+	</div>
+	<!--//banner-->
+	<!--welcome-->
+	<div class="welcome">
+		<div class="container">
+			<h3 class="title wow fadeInDown animated" data-wow-delay=".5s">Welcome To Our Site</h3>
+			<h4><p class="wow fadeInDown animated" data-wow-delay=".5s">Couturier, costume stylist, entrepreneur, social media influencer and revivalist. Elizabeth Chauhan’s prolific career spanning 26 years has seen her redefine fashion for an entire generation of Indians.</p>
+                        </h4><div class="welcome-info">
+				<div class="col-md-6 welcome-grids wow fadeInDown animated" data-wow-delay=".5s">
+					<div class="view view-eighth">
+						<img src="main/images/img1.jpg" alt="" class="img-responsive" />
+						<div class="mask">
+							<h2>Designer Ethnic Wear</h2>
+							<p>Visit Our Store To See Collections</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 welcome-grids wow fadeInDown animated" data-wow-delay=".5s">
+					<div class="view view-eighth">
+						<img src="main/images/img2.jpg" alt="" class="img-responsive" />
+						<div class="mask">
+							<h2>Designer Western Wear</h2>
+							<p>Visit Our Store To See Collections</p>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	<!--//welcome-->
+	<!--about-grids-->
+	<div class="about" id="about">
+		<div class="container">
+			<div class="about-top wow fadeInDown animated" data-wow-delay=".5s">	
+				<h4>Eliza Fashion Presents</h4>
+				<h2>FASHION FOR YOU </h2>
+				<div class="about-text">
+					<div class="col-md-6 abt-text-grids">
+						<p>&nbsp;</p>
+					</div>
+					<div class="col-md-6 abt-text-grids">
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<h5>CALL ME NOW :</h5>
+                                <a href="tel:+918849947902"><h3 class="wow zoomIn animated" data-wow-delay=".5s">+91 88499 47902</h3></a>
+			</div>
+			<div class="col-md-4 about-grids wow fadeInLeft animated" data-wow-delay=".5s">
+				
+				<p>Elizabeth Chauhan is a sustainable fashion label creating and manufacturing garments in-house, using ethical norms and practices. We believe in ‘Cradle-to- Cradle’ design or re-generative design which is holistic. We create systems that are not only efficient but also essentially waste free.</p>
+				<div class="more">
+					<a href="#">Get More >></a>
+				</div>
+				
+				<p>Recycling, upcycling and sustainable fashion is what we at Elizabeth's Design believe is the future and have established ourselves as a brand that has given resort wear a whole new dimension keeping sustainability and eco-friendly design at the heart and soul of the label.</p>
+				<div class="more">
+					<a href="#">Get More >></a>
+				</div>
+			</div>
+			<div class="col-md-4 about-grids wow fadeInUp animated" data-wow-delay=".5s">
+				<img src="main/images/2.png" alt="">
+			</div>
+			<div class="col-md-4 about-grids wow fadeInRight animated" data-wow-delay=".5s">
+				
+				<p>Our clothes are for the free- spirited world traveler who is bohemian at heart, dreaming of a world without boundaries… There is nothing more beautiful than a woman being unapologetically herself, comfortable in her perfect imperfection… that is the true essence of beauty. </p>
+				<div class="more">
+					<a href="#">Get More >></a>
+				</div>
+				
+				<p>Sustainability is an ideology, a state of mind and a concept close to my heart as a designer and a creative person. It is the crux and essence of my label and brand. Sustainability is about slow-fashion, time-less fashion, it’s about evolving a concept, re-inventing and re-interpreting it over a few seasons. Sustainable fashion does not believe in churning out collection after collection making the consumers feel that they are outdated because they don’t have the latest pieces. It is about adding separates to continue the evolution of a concept or design language.</p>
+				<div class="more">
+					<a href="#">Get More >></a>
+				</div>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+	<!--//about-grids-->
+	<!--services-->
+	<div class="services" id="services">
+		<div class="container">
+			<h3 class="title wow fadeInLeftBig animated" data-wow-delay=".5s">Our Production</h3>
+                        <h4><p class="text-right wow fadeInRight animated" data-wow-delay=".5s">&ldquo;Indians layered, draped, wrapped and tucked fabric, prints, colours   and more in an unabashed medley — a celebration of life and craft.    Surely we should retain that essence, that flavour in our contemporary   fashion — for that is our identity,  the layering of an old civilization   of thousands of years.&rdquo;</p></h4>
+			<p class="text-right wow fadeInRight animated" data-wow-delay=".5s">&nbsp;</p>
+			<div class="services-info">
+				<div class="col-md-6 services-left">
+					<div class="services-top wow fadeInLeft animated" data-wow-delay=".5s">
+						<div class="col-md-6 services-grids">
+							<img src="main/images/img3.jpg" alt="" class="img-responsive zoom-img" />
+						</div>
+						<div class="col-md-6 services-grids">
+							
+							<p>Eliza’s love for Indian culture has propelled her to undertake historical and mythological costuming works extensively , having involved a plethora of Indian craftsmen, ever since the inception of the company.</p>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="services-top bottom wow fadeInLeft animated" data-wow-delay=".5s">
+						<div class="col-md-6 services-grids srvc-img-right">
+							<img src="main/images/img4.jpg" alt="" class="img-responsive zoom-img" />
+						</div>
+						<div class="col-md-6 services-grids">
+							
+							<p>The studio has exhibited its works internationally and carried out workshops/lectures on Indian costume design in collaboration with foreign universities having made its present felt globally.</p>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+				</div>
+				<div class="col-md-6 services-left">
+					<div class="services-top wow fadeInRight animated" data-wow-delay=".5s">
+						<div class="col-md-6 services-grids">
+							<img src="main/images/img4.jpg" alt="" class="img-responsive zoom-img" />
+						</div>
+						<div class="col-md-6 services-grids">
+							
+							<p>Our global approach to the aesthetics of the label helps us create an eclectic fusion of both sensibilities thus expanding our possibilities and consumer base.</p>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="services-top bottom wow fadeInRight animated" data-wow-delay=".5s">
+						<div class="col-md-6 services-grids srvc-img-right">
+							<img src="main/images/img3.jpg" alt="" class="img-responsive zoom-img" />
+						</div>
+						<div class="col-md-6 services-grids">
+							
+							<p>This is our way to give back to the culture that has enriched our sensibilities with its heritage; this is our way to take only as much from the environment as much as we can give back.</p>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	<!--//services-->
+	<!--events-->
+	<div class="events">
+		<div class="container">
+			<h3 class="title wow fadeInDown animated" data-wow-delay=".5s">Events</h3>
+			<div class="events-info">
+				<div class="col-md-6 event-grids wow fadeInDown animated" data-wow-delay=".5s">
+					<div class="six">
+						<h4>0<span>0</span></h4>
+						<h6><a href="#">No Events </a></h6>
+						<div class="clearfix"> </div>
+					</div>
+					<p>Sorry, No Events Yet</p>
+					<div class="more">
+						<a href="#">Get More >></a>
+					</div>
+				</div>
+				<div class="col-md-6 event-grids wow fadeInDown animated" data-wow-delay=".5s">
+					<div class="six">
+						<h4>0<span>0</span></h4>
+						<h6><a href="#">No Events </a></h6>
+						<div class="clearfix"> </div>
+					</div>
+					<p>Sorry, No Events Yet</p>
+					<div class="more">
+						<a href="#">Get More >></a>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	<!--//events-->
+	
+	<!-- gallery -->
+	<div class="gallery" id="gallery">
+		<div class="container">
+			<h3 class="title wow fadeInLeft animated" data-wow-delay=".5s">Exclusive Designs</h3>
+			<h4><p class="text-right wow fadeInRight animated" data-wow-delay=".5s">Elizabeth Chauhan put up an incredible fashion show for the promotion of   Khadi, with cape-style jackets, fluid silhouette with statement   jewellery. The Gujarat State Khadi and Village Industries Board had   organized fashion show in collaboration with FDCI (Fashion Design   Council of India) to promote Khadi under Narendra Modi&rsquo;s &lsquo;Made in India&rsquo;   campaign.</p>			
+                        </h4><div class="gallery-grids">
+				<div class="gallery-grid">
+					<a href="main/images/img12.jpg" class="wow fadeInUp animated" data-wow-delay=".5s">
+						<img src="main/images/img12.jpg" alt=" " class="img-responsive zoom-img" />
+					</a>
+					<a href="main/images/img9.jpg" class="wow fadeInUp animated" data-wow-delay=".5s">
+						<img src="main/images/img9.jpg" alt=" " class="img-responsive zoom-img" />
+					</a>
+				</div>
+				<div class="gallery-grid">
+					<a href="main/images/img11.jpg" class="wow fadeInUp animated" data-wow-delay=".5s">
+						<img src="main/images/img11.jpg" alt=" " class="img-responsive zoom-img" />
+					</a>
+					<a href="main/images/img10.jpg" class="wow fadeInUp animated" data-wow-delay=".5s">
+						<img src="main/images/img10.jpg" alt=" " class="img-responsive zoom-img" />
+					</a>
+				</div>
+				<div class="gallery-grid">
+					<a href="main/images/img12.jpg" class="wow fadeInUp animated" data-wow-delay=".5s">
+						<img src="main/images/img12.jpg" alt=" " class="img-responsive zoom-img" />
+					</a>
+					<a href="main/images/img9.jpg" class="wow fadeInUp animated" data-wow-delay=".5s">
+						<img src="main/images/img9.jpg" alt=" " class="img-responsive zoom-img" />
+					</a>
+				</div>
+				<div class="gallery-grid" class="wow fadeInUp animated" data-wow-delay=".5s">
+					<a href="main/images/img11.jpg">
+						<img src="main/images/img11.jpg" alt=" " class="img-responsive zoom-img" />
+					</a>
+					<a href="main/images/img10.jpg" class="wow fadeInUp animated" data-wow-delay=".5s">
+						<img src="main/images/img10.jpg" alt=" " class="img-responsive zoom-img" />
+					</a>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	<!--//gallery -->
+	
+	<!--contact-->
+	<div class="contact"  id="contact">
+		<div class="container">
+			<div class="contact-grids">
+				<div class="col-md-5 contact-grid">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58784.18455615422!2d72.58531344375227!3d22.949802307298633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e889e9f447065%3A0x75a855584b5baf08!2sVatva%2C+Ahmedabad%2C+Gujarat!5e0!3m2!1sen!2sin!4v1496829621673" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>				</div><div class="col-md-4 contact-grid">
+					<h3 class="title wow fadeInDown animated" data-wow-delay=".5s">Contact Us</h3>
+					<form class="wow fadeInUp animated" data-wow-delay=".5s">
+						<input type="text" value="Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name';}" required="">
+						<input type="email" value="Email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email';}" required="">
+						<input type="text" value="Telephone" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Telephone';}" required="">
+						<textarea type="text"  onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Message...';}" required="">Message...</textarea>
+						<input type="submit" value="Send" >
+					</form>
+				</div>
+				<div class="col-md-3 contact-grid wow fadeInRight animated" data-wow-delay=".5s">
+					<h3 class="title">Address</h3>
+					<ul>
+                                            <li class="dot"><i class="glyphicon glyphicon-map-marker" aria-hidden="true"></i>67. B. 309/5. HariKrishna Park<span>
+				<p>B/s Ranchhodnagar, Opp. Urmijivan Soc</p>
+                                <p>Vatva Vinzol Road, Vatva, Ahmedabad.</p></span></li>
+						<li class="Phone"><i class="glyphicon glyphicon-phone" aria-hidden="true"></i>+91 88499 47902</li>
+						<li class="mail"><i class="glyphicon glyphicon-envelope" aria-hidden="true"></i><a href="mailto:info@example.com" class="mail">Example.com</a></li>
+					</ul>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	<!--//contact-->
+	<!--footer-->
+	<div class="footer">
+		<div class="footer-info">
+			<div class="container">	
+				<div class="icons wow fadeInDown animated" data-wow-delay=".5s">	
+					<ul>
+						<li><a href="#" data-toggle="tooltip" data-placement="top" title="Facebook"><img src="main/images/i1.png" alt=""/></a></li>
+						<li><a href="#" data-toggle="tooltip" data-placement="top" title="Twitter"><img src="main/images/i2.png" alt=""/> </a></li>
+						<li><a href="#" data-toggle="tooltip" data-placement="top" title="Google+"><img src="main/images/i3.png" alt=""/></a></li>
+						<li><a href="#" data-toggle="tooltip" data-placement="top" title="LinkedIn"><img src="main/images/i4.png" alt=""/> </a></li>
+						<li><a href="#" data-toggle="tooltip" data-placement="top" title="Pinterest"><img src="main/images/i5.png" alt=""/> </a></li>
+					</ul>
+					<script>$(function () {
+					  $('[data-toggle="tooltip"]').tooltip()
+					})</script>
+				</div>
+				<p class="wow fadeInUpBig animated" data-wow-delay=".5s">© 2017 Alexevan . All rights reserved | Design by <a href="http://alexchristian.comuv.com">Alex Christian</a></p>					
+			</div>
+		</div>
+	</div>
+	<!--//footer-->
+	<!--smooth-scrolling-of-move-up-->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+			var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+			};
+			*/
+			
+			$().UItoTop({ easingType: 'easeOutQuart' });
+			
+		});
+	</script>
+	<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+	<!--//smooth-scrolling-of-move-up-->
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="main/js/bootstrap.js"></script>
+</body>
+</html>
